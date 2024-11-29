3735576c9cc4eca29d42befc13de77793d5d4383
Dinara Usenakunova <ditiviola@gmail.com>
2024-11-29 21:14:26 +0300

    Express Tutorial Part 4: Routes and controllers

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog");
 
 const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-  " -_- "
+const mongoDB = " -_- ";
+// var mongoDB = "mongodb://127.0.0.1/my_database";
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
